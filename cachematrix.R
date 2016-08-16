6c0e18a0d6ee33f3229b57b048bd908edbb6deb7
Ryno8Gtr <radees008@yahoo.com>
2016-08-16 18:17:17 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,35 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+function(x, ...) { 
+    ## Return a matrix that is the inverse of 'x' 
+    inv <- x$getinv()
+    if(!is.null(inv)){
+        message("getting cache inverse")
+        return(inv)
+    }
+    mat <- x$get()
+    ## matrix singularity not tested (supposed invertible)
+    inv <- solve(mat)
+    x$setinv(inv)
+    inv
+}
+function(x = matrix()) { 
+    ## initiate the inverse to null
+    i <- NULL
+    ## loads matrix from the parent environment
+    set <- function(y){
+        x <<- y
+        ## initiate the inverse to null
+        i <- NULL
+    }
+    ## returns the matrix
+    get <- function() x
+    # stores the inverse. Will return an error if the matrix is singular
+    setinv <- function(solve) i<<-solve
+    # returns the cached inverse. Will return NULL if cache empty
+    getinv <- function() i
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
+    
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
